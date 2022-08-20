23f9a41e943d0a43d87d6b3d9b911d2a57705a90
BufferOverflow <scharxidev@outlook.de>
2022-08-20 21:14:45 +0200

    Initial Commit 🚀

---
+++ palindrom-checker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+string ReplaceAllSpecialCharacters(ref string input)
+{
+    var reg = new Regex("[!+-/*'\",_&#^\\\\@$]");
+    return reg.Replace(input, string.Empty);
+}
+
+Tuple<string, bool> IsPalindrome(string input)
+{
+    var str = ReplaceAllSpecialCharacters(ref input);
+    return Tuple.Create(str, string.Equals(str, string.Join("", str.Reverse().ToList()), StringComparison.OrdinalIgnoreCase));
+}
+
+Console.Write("Input the word you want to check: ");
+Console.WriteLine($"Palindrome: {IsPalindrome(Console.ReadLine() ?? throw new InvalidOperationException()).Item2}");
